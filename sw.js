742928b768b2961eedca0e8c3badf3fd5172f1fc
melendezantonio <65402473+melendezantonio@users.noreply.github.com>
2025-08-27 15:01:17 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -7,8 +7,16 @@ self.addEventListener('install', (e) => {
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
